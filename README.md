c877eb1e0d2a3b47685dd89830d3549fc57c56dc
Emanuele <manuncorrea@hotmail.com>
2020-08-22 22:53:22 -0300

    Connecting the ReactJs application to the backend

---
+++ README.md
@@ -0,0 +1,15 @@
+## 🚀 Desafaio conceitos ReactJs
+
+## Instalar dependencias necessarias
+ yarn intal
+
+## Intruções:
+  Conectar a aplicação ReactJs no backend do desafio anterior 
+
+## Funcionalidades da aplicação
+  ° Listar os repositórios da sua API: Deve ser capaz de criar uma lista com o campo title de todos os repositórios que estão cadastrados na sua API.
+
+  °Adicionar um repositório a sua API: Deve ser capaz de adicionar um novo item na sua API através de um botão com o texto Adicionar e, após a 
+  criação, deve ser capaz de exibir o nome dele após o cadastro.
+
+  ° Remover um repositório da sua API: Para cada item da sua lista, deve possuir um botão com o texto Remover que, ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  //Chamada a apai
+  useEffect(() => {
+    //Adicionando a rota
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    // Adicionando novo repositorio
+    const response = await api.post('repositories', {
+      title: `GooStack`,
+      url: 'https://github.com/manuncorrea/',
+      teches: ["ReactJs", "NodeJS"]
+    });
+
+    //const repository = response.data;
+
+    setRepositories([...repositories, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id // para cada um dos repoositorios verificar de o id e diferente do que foi removido
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
